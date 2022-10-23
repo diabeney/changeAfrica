c50ac6f62fa18e16240917f71cae01e6d1b9d302
Diabene Addo <addodiabene69@gmail.com>
2022-10-23 18:14:43 +0000

    fixed bug

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,3 +1,11 @@
-import { menuBtnHandler } from "../Utils/Helpers";
+import { getElement, menuBtnHandler } from "../Utils/Helpers";
 
 menuBtnHandler();
+
+const [contactElement] = getElement("#contact");
+
+contactElement.addEventListener("click", (e) => {
+  e.preventDefault();
+  const [footer] = getElement("footer");
+  footer.scrollIntoView();
+});
